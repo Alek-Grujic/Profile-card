1499b94fcf684a6b2f68da2d00bbbacdb2fc77e8
Alek-Grujic <alek.grujic001@outlook.com>
2026-04-15 21:26:33 +0200

    init: build profile card app with React and Vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+const skillList = [
+  {
+    name: "HTML+CSS",
+    emoji: "💪",
+    color: "#2457E4",
+  },
+  {
+    name: "JavaScript",
+    emoji: "💪",
+    color: "#EBD317",
+  },
+  {
+    name: "Web Design",
+    emoji: "💪",
+    color: "#B9D7A3",
+  },
+  {
+    name: "Git and GitHub",
+    emoji: "👍",
+    color: "#E3462D",
+  },
+  {
+    name: "React",
+    emoji: "💪",
+    color: "#53D4F7",
+  },
+  {
+    name: "Svelte",
+    emoji: "👶",
+    color: "#FD3200",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
 }
 
-export default App
+function Avatar() {
+  return (
+    <div className="avatarWrapper">
+      <img src="./public/prof.png" alt="#" className="avatar" />
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jonas Schmedtmann</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure soluta
+        neque aliquid dignissimos animi voluptates natus culpa? Temporibus,
+        veniam neque, officia totam.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <ul className="skill-list">
+      {skillList.map((skill) => (
+        <Skill
+          key={skill.name}
+          name={skill.name}
+          emoji={skill.emoji}
+          color={skill.color}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Skill(props) {
+  return (
+    <li style={{ background: props.color }} className="skill">
+      {`${props.name} ${props.emoji}`}
+    </li>
+  );
+}
+
+export default App;
